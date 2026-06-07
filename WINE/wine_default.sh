34770f5299325ba448e923394a18311edbd01bbe
Nekotato1412 <nekotato1412@gmail.com>
2026-06-07 08:07:08 -0500

    WINE Defaults

---
+++ WINE/wine_default.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+echo "Creating a launcher for WINE.."
+cp ./wine.desktop ~/.local/share/applications/wine.desktop
+sudo chmod +x ~/.local/share/applications/wine.desktop
+sudo cp ./wine.desktop /usr/share/applications/wine.desktop
+sudo chmod +x /usr/share/applications/wine.desktop
+
+cp ./wine-open.nemo_action ~/.local/share/nemo/actions/wine-open.nemo_action
+sudo chmod +x ~/.local/share/nemo/actions/wine-open.nemo_action
+
+echo "Making WINE the default application.."
+sudo xdg-mime default ~/.local/share/applications/wine.desktop application/x-ms-dos-executable
+sudo xdg-mime default ~/.local/share/applications/wine.desktop application/x-msdownload
+echo "OK."
+read -p "Hit ENTER to refresh and close."
+update-desktop-database ~/.local/share/applications/
+update-mime-database ~/.local/share/mime/
+nemo -q && nemo &
